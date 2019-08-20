c4f20b37ea28ea82a2235e9c064fbe73804b7a53
Oscar Gomez <50819676+termux-io@users.noreply.github.com>
2019-08-20 08:34:37 -0700

    Revert "loading numpy during build - not before"

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
-from distutils.core import setup
-from setuptools import Extension
-import sys
 import logging
+import sys
 
 logging.basicConfig()
 log = logging.getLogger()
 
 try:
-    from Cython.Distutils import build_ext
-    ext_modules = [Extension("jenks", ["jenks.pyx"])]
+    import Cython
 except ImportError:
-    from setuptools.command.build_ext import build_ext
-    ext_modules = [Extension("jenks", ["jenks.c"])]
+    log.critical(
+        "Cython is required to run setup(). Exiting.")
+    sys.exit(1)
+else:
+    from Cython.Distutils import build_ext
 
+from setuptools import setup
+from distutils.extension import Extension
 
-class CustomBuildExtCommand(build_ext):
-    """build_ext command for use when numpy headers are needed."""
-    def run(self):
-        # Import numpy here, only when headers are needed
-        try:
-            import numpy
-        except ImportError:
-            log.critical("Numpy is required to run setup(). Exiting.")
-            sys.exit(1)
-        # Add numpy headers to include_dirs
-        self.include_dirs.append(numpy.get_include())
-        # Call original build_ext command
-        build_ext.run(self)
+include_dirs = []
 
+try:
+    import numpy
+    include_dirs.append(numpy.get_include())
+except ImportError:
+    log.critical(
+        "Numpy and its headers are required to run setup(). Exiting.")
+    sys.exit(1)
 
 setup(
     name="jenks",
@@ -38,8 +35,8 @@ setup(
     license="BSD",
     keywords="gis geospatial geographic statistics numpy cython choropleth",
     url="https://github.com/perrygeo/jenks",
-    install_requires=['numpy', 'cython'],
-    setup_requires=['numpy', 'cython'],
-    cmdclass={'build_ext': CustomBuildExtCommand},
-    ext_modules=ext_modules
+    install_requires=['Numpy'],
+    cmdclass={'build_ext': build_ext},
+    ext_modules = [
+        Extension("jenks", ["jenks.pyx"], include_dirs=include_dirs)]
 )
